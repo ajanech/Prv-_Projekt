dad5482f05a60130c8bff3990c4139e2af609cf8
lkrabac <lkrabac1@gmail.com>
2023-01-13 00:14:45 +0100

    dokoncil som Login js

--- login.js
+++ login.js
@@ -3,9 +3,18 @@
     var login = $('.login-popup');
     var loginbutton = $('.login-button');
 
+    login.hide();
+    var hidden = true;
+
     loginbutton.on('click', function(event) {
-        login.toggleClass('hidden');
-        console.log('class');
+        if(hidden) {
+            login.fadeIn(500);
+            hidden = false;
+        }
+        else {
+            login.fadeOut(500);
+            hidden = true;
+        }
     });
 
 })(jQuery);
